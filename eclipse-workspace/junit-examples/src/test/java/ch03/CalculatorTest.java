e21cbe12b79d90ff0dedae622f3ce6776bea3a1b
Takuya-Endo <above5purple@gmail.com>
2022-01-02 10:05:17 +0900

    [Update]JUnitPractice-8

---
+++ eclipse-workspace/junit-examples/src/test/java/ch03/CalculatorTest.java
@@ -0,0 +1,27 @@
+package ch03;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Ignore
+	@Test
+	public void testDivide() throws Exception {
+		
+		//SetUp - 初期化
+		Calculator sut = new Calculator();
+		
+		//Exercise - テスト実行
+		int actual = sut.divide(2, 3);
+		
+		//Verify - 検証
+		int expected = 0;
+		assertThat(actual, is(expected));
+		
+	}
+
+}
