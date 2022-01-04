ded375f2de97efa62670daadbb21aa9764c357f5
Takuya-Endo <above5purple@gmail.com>
2022-01-05 04:49:03 +0900

    [Update]JUnitPractice-17

--- eclipse-workspace/junit-examples/src/test/java/ch11/ArrayListTest.java
+++ eclipse-workspace/junit-examples/src/test/java/ch11/ArrayListTest.java
@@ -5,25 +5,32 @@ import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ArrayListTest {
 
+	//SUT - テスト対象システム
+	private ArrayList<String> sut;
 	
+	@Before
+	public void setUp() {
+		
+		//SetUp - 事前処理
+		this.sut = new ArrayList<String>();
+		this.sut.add("A");
+		this.sut.add("B");
+		this.sut.add("C");
+		this.sut.add("D");
+		this.sut.add("E");
+		
+	}
 	
 	@Test
 	public void test() {
 		
-		//SetUp - 事前処理
-		ArrayList<String> sut = new ArrayList<String>();
-		sut.add("A");
-		sut.add("B");
-		sut.add("C");
-		sut.add("D");
-		sut.add("E");
-		
 		//Exercise - テスト実行
-		int actual = sut.size();
+		int actual = this.sut.size();
 		
 		//Verify - 検証
 		int expected = 5;
