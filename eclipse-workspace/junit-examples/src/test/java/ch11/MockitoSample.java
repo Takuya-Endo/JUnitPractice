6a93e7eda95ade86be8f758b4b1c5cc1364e6209
Takuya-Endo <above5purple@gmail.com>
2022-02-01 07:13:02 +0900

    [Update]JUnitPractice-25

--- eclipse-workspace/junit-examples/src/test/java/ch11/MockitoSample.java
+++ eclipse-workspace/junit-examples/src/test/java/ch11/MockitoSample.java
@@ -4,6 +4,8 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -12,13 +14,10 @@ public class MockitoSample {
 	@Test
 	public void testMockitoSample() {
 	
-//		List<String> mock = Mockito.mock(List.class);
-//		
-//		assertThat(mock.get(0), is(nullValue()));
-//		assertThat(mock.contains("Hello"), is(false));
-		
-		User mock = Mockito.mock(User.class);
-		assertThat(mock, is(nullValue()));
+		//初め動かなくて、VM引数試して、動かなくて、消して再度試したら成功した。。
+		List<String> mock = Mockito.mock(List.class);
+		assertThat(mock.get(0), is(nullValue()));
+		assertThat(mock.contains("Hello"), is(false));
 		
 	}
 	
